991720dacf1ac49928e3494b95e0c1bd4bcd831b
Alex Nilga <alexnilga@gmail.com>
2018-05-29 21:15:45 +0300

    Add new journal button has been added

---
+++ SchoolJournal/Pages/AddJournal.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using SchoolJournal.Repositories;
+
+namespace SchoolJournal.Pages
+{
+    public class AddJournalModel : PageModel
+    {
+        private readonly InMemoryRepository _repository;
+
+        public AddJournalModel()
+        {
+            _repository = new InMemoryRepository();
+        }
+
+        public string Title => "Add Journal";
+
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
--- SchoolJournal/Pages/Index.cshtml.cs
+++ SchoolJournal/Pages/Index.cshtml.cs
@@ -29,5 +29,6 @@ namespace SchoolJournal.Pages
 
             return Page();
         }
+
     }
 }
