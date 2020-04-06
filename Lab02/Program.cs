b7c05594c0b466c15d8a9a19cdb822507a18fee4
Nick Nilga <myko@telenor.dk>
2020-04-06 21:15:38 +0200

    Setup the goal for lab 02

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -6,7 +6,41 @@ namespace Lab02
 	{
 		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.Out.WriteLine("Calculation of the roots of the quadratic equation");
+			Console.Out.WriteLine();
+			
+			// Define variables for keeping a, b, and c - coefficients of equation.
+			// Note that they can be with decimal parts, so int is not a suitable type.
+			
+			
+			// Ask the user to enter a by printing invitation to the console
+			Console.Out.Write("Input a and press Enter: ");
+			
+			// Read string value entered by the user.
+			// <type> strA = Console.In.ReadLine();
+			
+			// Do the same for b and c
+			
+			
+			// Define a variable for keeping discriminant and calculate it' value.
+			
+			// Output discriminant value to the console like this: "d = 3242"
+			
+			// Analyze discriminant. Put if...else block like this
+			// if (d>0)
+			// {
+			//   calculate roots
+			// }
+			// else if (d == 0)
+			// {
+			// calculate root
+			// }
+			// else
+			// {
+			//   write a message that there are no roots.
+			// }
+			//
+			
 		}
 	}
 }
