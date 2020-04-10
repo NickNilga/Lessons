fe7dc6d3ca00dc61dd43060101bde51315ab3c80
Nick Nilga <myko@telenor.dk>
2020-04-10 23:04:58 +0300

    Task for arrays has been added

---
+++ Lab03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab03
+{
+	// Arrays
+	internal static class Program
+	{
+		private static void Main()
+		{
+			int[] arrayA = new[] {1, 2, 3, 4, 5, 6};
+
+			int[] arrayB = new int[10];
+
+			arrayB[0] = 45;
+			arrayB[1] = 53;
+			arrayB[2] = 25;
+			arrayB[3] = 41;
+			arrayB[4] = -8;
+			arrayB[5] = 87;
+			arrayB[6] = 4;
+			arrayB[7] = -5;
+			arrayB[8] = 465;
+			arrayB[9] = 32;
+
+			for (int i = 0; i < arrayA.Length; i++)
+			{
+				Console.Out.WriteLine("array [{0}] = {1}", i, arrayA[i]);
+			}
+
+			foreach (int b in arrayB)
+			{
+				Console.Out.WriteLine("b = {0}", b);
+			}
+		}
+	}
+}
