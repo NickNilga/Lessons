0a68914d1f1f0f83f17253e322c4e58510b05e7e
Nick Nilga <myko@telenor.dk>
2020-05-10 18:45:41 +0300

    cleanup

--- Lab04/Car.cs
+++ Lab04/Car.cs
@@ -3,71 +3,69 @@ using NOD;
 
 namespace Lab04
 {
-    public class Car
-    {
-        private string _model;
-        private string _make;
-        private Engine _engine;
-        private Wheel[] _wheels;
-        private Radio _radio;
+	public class Car
+	{
+		private string _model;
+		private string _make;
+		private Engine _engine;
+		private Wheel[] _wheels;
+		private Radio _radio;
 
-        public Car(
-            string model,
-            string make,
-            Engine engine,
-            string wheelModel,
-            int wheelRadius,
-            int wheelWidth)
-        {
-            _model = model;
-            _make = make;
-            _engine = engine;
+		public Car(
+			string model,
+			string make,
+			Engine engine,
+			string wheelModel,
+			int wheelRadius,
+			int wheelWidth)
+		{
+			_model = model;
+			_make = make;
+			_engine = engine;
 
-            _wheels = new Wheel[4];
+			_wheels = new Wheel[4];
 
-            for (int i = 0; i < 4; i++)
-            {
-                _wheels[i] = new Wheel(wheelModel, wheelRadius, wheelWidth);
-            }
-        }
+			for (int i = 0; i < 4; i++)
+			{
+				_wheels[i] = new Wheel(wheelModel, wheelRadius, wheelWidth);
+			}
+		}
 
-        public override string ToString()
-        {
-            string result = $"Car: {_make} model{_model}\n" +
-                            $"Engine: {_engine}\n" +
-                            $"Wheels: {_wheels[0]}\n";
+		public override string ToString()
+		{
+			string result = $"Car: {_make} model{_model}\n" +
+			                $"Engine: {_engine}\n" +
+			                $"Wheels: {_wheels[0]}\n";
 
-            string radioString = "Radio: "  + (_radio == null ? "none" : _radio.ToString());
-            result += radioString;
+			string radioString = "Radio: " + (_radio == null ? "none" : _radio.ToString());
+			result += radioString;
 
-            return result;
-        }
+			return result;
+		}
 
-        public void SetRadio(Radio radio)
-        {
-            _radio = radio;
-            
-        }
+		public void SetRadio(Radio radio)
+		{
+			_radio = radio;
+		}
 
-        public void RemoveRadio()
-        {
-            _radio = null;
-        }
+		public void RemoveRadio()
+		{
+			_radio = null;
+		}
 
-        public void StartEngine()
-        {
-            _engine.Start();
-        }
+		public void StartEngine()
+		{
+			_engine.Start();
+		}
 
-        public void StopEngine()
-        {
-            _engine.Stop();
-        }
-        
-        public bool IsStarted()
-        {
-            return _engine.IsStarted();
-        }
+		public void StopEngine()
+		{
+			_engine.Stop();
+		}
 
-    }
+		public bool IsStarted()
+		{
+			return _engine.IsStarted();
+		}
+	}
 }
--- Lab04/Wheel.cs
+++ Lab04/Wheel.cs
@@ -2,23 +2,23 @@ using System;
 
 namespace Lab04
 {
-    public class Wheel
-    {
-        private string _model;
-        private int _radius;
-        private int _width;
+	public class Wheel
+	{
+		private string _model;
+		private int _radius;
+		private int _width;
 
 
-        public Wheel(string model, int radius, int width)
-        {
-            _model = model;
-            _radius = radius;
-            _width = width;
-        }
-        
-        public override string ToString()
-        {
-            return $"Model {_model}; R={_radius}''; W={_width}mm";
-        }
-    }
+		public Wheel(string model, int radius, int width)
+		{
+			_model = model;
+			_radius = radius;
+			_width = width;
+		}
+
+		public override string ToString()
+		{
+			return $"Model {_model}; R={_radius}''; W={_width}mm";
+		}
+	}
 }
