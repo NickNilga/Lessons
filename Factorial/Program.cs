77396f6842be1cf3fe3397a90bc40b10bb1e9c26
Nick Nilga <myko@telenor.dk>
2020-05-15 10:27:15 +0300

    Code cleanup

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -1,49 +1,44 @@
 ﻿using System;
-using System.Transactions;
 
 namespace Factorial
 {
-    class Program
-    {
-        static void Main()
-        {
-            int x = GetX();
-            Console.Out.WriteLine("Factoial:{0}", CalculateFactorial(x));
-            Console.Out.WriteLine("Factoial:{0}", CalculateFactorial2(x));
-        }
+	internal static class Program
+	{
+		static void Main()
+		{
+			int x = GetX();
+			Console.Out.WriteLine("{0}! = {1}", x, CalculateFactorial(x));
+			Console.Out.WriteLine("{0}! = {1}", x, CalculateFactorialRecursively(x));
+		}
 
-        private static int GetX()
-        {
-            int factorial;
-            
-            Console.Out.Write("Enter Factorial:");
-            string strNumber = Console.In.ReadLine();
-            int.TryParse(strNumber, out factorial);
-            
-            return factorial;
-        }
+		private static int GetX()
+		{
+			int factorial;
 
-        private static int CalculateFactorial(int a)
-        {
-            int result = 1;
-            while(a > 0)
-            {
-                result *= a;
-                a -= 1;
-            }
+			Console.Out.Write("Enter Factorial:");
+			string strNumber = Console.In.ReadLine();
+			int.TryParse(strNumber, out factorial);
 
-            return result;
-        }
+			return factorial;
+		}
 
-        private static int CalculateFactorial2(int a)
-        {
-            return a == 0 
-                ? 1 
-                : a * CalculateFactorial2(a - 1);
-        }
-    }
+		private static int CalculateFactorial(int a)
+		{
+			int result = 1;
+			while (a > 0)
+			{
+				result *= a;
+				a -= 1;
+			}
+
+			return result;
+		}
+
+		private static int CalculateFactorialRecursively(int a)
+		{
+			return a == 0
+				? 1
+				: a * CalculateFactorialRecursively(a - 1);
+		}
+	}
 }
-
-
-    
-    
--- Lab03/Program.cs
+++ Lab03/Program.cs
@@ -7,66 +7,64 @@ namespace Lab03
 	{
 		private static void Main()
 		{
-			AnotherFunction(1.0f);
 			int[] a = LoadArray();
 			int[] result = FindMax(a);
 			double avg = FindAvg(a);
-			Console.Out.WriteLine("Max = {0}; max index = {1}; Avg = {2}", result[0],result[1], avg);
-			
+			Console.Out.WriteLine("Max = {0}; max index = {1}; Avg = {2}", result[0], result[1], avg);
 		}
 
 		private static int[] LoadArray()
 		{
 			int numberOfElements;
-			
+
 			Console.Out.Write("Enter the number or elements:");
 			string strNumber = Console.In.ReadLine();
 			int.TryParse(strNumber, out numberOfElements);
-			
+
 			int[] a = new int[numberOfElements];
-			
+
 			for (int i = 0; i < a.Length; i++)
 			{
 				Console.Out.Write("Enter {0} value: ", i + 1);
-				string str = Console.In.ReadLine();
+				string str = Console.In.ReadLine() ?? "0";
 				a[i] = int.Parse(str);
 			}
 
 			Console.Out.Write("Array: [");
 			for (int i = 0; i < a.Length; i++)
 			{
-				if(i < a.Length - 1)
+				if (i < a.Length - 1)
 				{
-					Console.Out.Write("{0}, ", a[i]);	
+					Console.Out.Write("{0}, ", a[i]);
 				}
 				else
 				{
-					Console.Out.Write("{0}", a[i]);	
+					Console.Out.Write("{0}", a[i]);
 				}
-				
 			}
+
 			Console.Out.WriteLine("]");
 
 			Console.Out.WriteLine($"Array: [{string.Join(", ", a)}]");
-			
+
 			return a;
 		}
-		
+
 
 		private static int[] FindMax(int[] array)
 		{
 			int max = array[0];
-			int maxindex = 0;
-			for (int i = 1; i < array.Length; i ++)
+			int maxIndex = 0;
+			for (int i = 1; i < array.Length; i++)
 			{
 				if (array[i] > max)
 				{
 					max = array[i];
-					maxindex = i;
+					maxIndex = i;
 				}
 			}
 
-			int[] result = {max, maxindex};
+			int[] result = {max, maxIndex};
 
 			return result;
 		}
@@ -77,21 +75,9 @@ namespace Lab03
 			for (int i = 0; i < array.Length; i++)
 			{
 				sum += array[i];
-				
 			}
-			
+
 			return sum / array.Length;
-
 		}
-		private static void AnotherFunction(int x)
-		{
-		}
-
-		private static void AnotherFunction(float x)
-		{
-		}
-		
-		
-
 	}
 }
