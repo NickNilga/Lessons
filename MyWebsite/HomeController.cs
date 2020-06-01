609e10d2f35a688c9108309deba80abb692d2c7c
Nick Nilga <myko@telenor.dk>
2020-06-01 21:35:16 +0300

    Add MVC to the website

--- MyWebsite/HomeController.cs
+++ MyWebsite/HomeController.cs
@@ -2,13 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyWebsite
 {
-    public class HomeController : Controller
-    {
-        // GET
-        public IActionResult Index()
-        {
-            return Content("Hi there");
-            // return View();
-        }
-    }
+	public class HomeController : Controller
+	{
+		// GET
+		public IActionResult Index()
+		{
+			return Content("Hi there");
+		}
+	}
 }
